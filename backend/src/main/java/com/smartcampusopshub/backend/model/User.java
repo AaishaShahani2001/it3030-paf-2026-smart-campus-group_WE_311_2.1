2b49e5aecfcef36b843c01dae4010b17c43e209d
DheenaAmjath <dheenaamjath8@gmail.com>
2026-04-01 13:02:10 +0530

    added User entity

---
+++ backend/src/main/java/com/smartcampusopshub/backend/model/User.java
@@ -0,0 +1,25 @@
+package com.smartcampusopshub.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
